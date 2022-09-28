fedc80e8055dba3a08b953dab2983821370c70e2
Katerine <frna338@gmail.com>
2022-09-28 17:34:08 +0300

    Task13

---
+++ Задача13/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+
+System.Console.WriteLine("enter the number: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number > 99){
+    
+}
